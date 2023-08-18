0caa2920836894d762f1093cdf7acb4b60c66247
Sourajit20 <140809138+Sourajit20@users.noreply.github.com>
2023-08-19 04:13:16 +0530

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -17,7 +17,7 @@ st.sidebar.markdown(
     "<h2 style='color: #EEA637; font-weight: bold;'>Parameter Settings</h2>",
     unsafe_allow_html=True,
 )
-st.sidebar.markdown("Adjust the parameters to see changes in the histogram:")
+st.sidebar.markdown("Fine-tune the parameters to visualize the distribution:")
 
 # Get user input
 mean = st.sidebar.number_input("Mean", value=0.0)
@@ -29,7 +29,7 @@ np.random.seed(0)  # For reproducibility
 data = np.random.normal(mean, std_dev, num_samples)
 
 # Plot styling
-fig, ax = plt.subplots(figsize=(8, 6))
+fig, ax = plt.subplots(figsize=(10, 6))
 ax.hist(data, bins=30, edgecolor="k", color="#EEA637", alpha=0.7)
 ax.set_xlabel("Value", fontsize=14, fontweight="bold")
 ax.set_ylabel("Frequency", fontsize=14, fontweight="bold")
