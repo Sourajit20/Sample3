1947e62a6d7328cb618c5e7e426a0e88e35c9998
Sourajit20 <140809138+Sourajit20@users.noreply.github.com>
2023-08-19 02:43:45 +0530

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -3,11 +3,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Page layout
-st.set_page_config(page_title="Normal Distribution App", layout="wide")
+# Set custom page configuration
+st.set_page_config(
+    page_title="Normal Distribution App",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_icon=":chart_with_upwards_trend:",
+)
 
-# Sidebar
+# Sidebar styling
 st.sidebar.header("Parameters")
+st.sidebar.markdown("## <span style='color: #EEA637;'>Parameter Settings</span>", unsafe_allow_html=True)
+
+# Get user input
 mean = st.sidebar.number_input("Mean", value=0.0)
 std_dev = st.sidebar.number_input("Standard Deviation", value=1.0)
 num_samples = st.sidebar.number_input("Number of Samples", value=1000)
@@ -16,19 +24,30 @@ num_samples = st.sidebar.number_input("Number of Samples", value=1000)
 np.random.seed(0)  # For reproducibility
 data = np.random.normal(mean, std_dev, num_samples)
 
-# Create a figure and axis for the histogram
-fig, ax = plt.subplots()
-ax.hist(data, bins=30, edgecolor="k")
-ax.set_xlabel("Value")
-ax.set_ylabel("Frequency")
-ax.set_title("Histogram of Generated Data")
+# Plot styling
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.hist(data, bins=30, edgecolor="k", color="#EEA637")
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Frequency", fontsize=14)
+ax.set_title("Histogram of Generated Normal Distribution", fontsize=16)
 
 # Display the plot using st.pyplot()
-st.header("Histogram of Generated Data")
+st.markdown(
+    "<h1 style='text-align:center; color:#EEA637;'>Histogram of Generated Data</h1>",
+    unsafe_allow_html=True,
+)
 st.pyplot(fig)
 
-# Download as CSV
+# Button styling
 if st.button("Download Data as CSV"):
     df = pd.DataFrame(data, columns=["Value"])
     csv = df.to_csv(index=False)
-    st.download_button("Download CSV", data=csv, file_name="generated_data.csv")
+    st.download_button(
+        "Download CSV",
+        data=csv,
+        file_name="generated_data.csv",
+    )
+
+# App footer
+st.markdown("---")
+st.markdown("Created by Sourajit Ghosh :smile:")
