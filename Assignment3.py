e6def3643ac8cc334feafd3ee12b8dd9108fe0d4
Sourajit20 <140809138+Sourajit20@users.noreply.github.com>
2023-08-18 20:01:40 +0530

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(
     page_title="Normal Distribution App",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
     page_icon=":chart_with_upwards_trend:",
 )
 
