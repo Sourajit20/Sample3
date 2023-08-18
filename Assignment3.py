beb441d95d4435512d94e78a844a892369b7a53c
Sourajit20 <140809138+Sourajit20@users.noreply.github.com>
2023-08-18 19:48:27 +0530

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -3,11 +3,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Page layout
-st.set_page_config(page_title="Normal Distribution App", layout="wide")
+# Set custom page configuration
+st.set_page_config(
+    page_title="Normal Distribution App",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_icon=":chart_with_upwards_trend:",
+)
 
-# Sidebar
+# Sidebar styling
 st.sidebar.header("Parameters")
+st.sidebar.markdown(
+    "<h2 style='text-align:center; color:#EEA637;'>Parameter Settings</h2>",
+    unsafe_allow_html=True,
+)
+
+# Get user input
 mean = st.sidebar.number_input("Mean", value=0.0)
 std_dev = st.sidebar.number_input("Standard Deviation", value=1.0)
 num_samples = st.sidebar.number_input("Number of Samples", value=1000)
@@ -16,23 +27,31 @@ num_samples = st.sidebar.number_input("Number of Samples", value=1000)
 np.random.seed(0)  # For reproducibility
 data = np.random.normal(mean, std_dev, num_samples)
 
-# Create a figure and axis for the histogram
-fig, ax = plt.subplots()
+# Plot styling
+fig, ax = plt.subplots(figsize=(8, 6))
 ax.hist(data, bins=30, edgecolor="k", color="#EEA637")
-ax.set_xlabel("Value")
-ax.set_ylabel("Frequency")
-ax.set_title("Histogram of Generated Normal Distribution")
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Frequency", fontsize=14)
+ax.set_title("Histogram of Generated Normal Distribution", fontsize=16)
 
 # Display the plot using st.pyplot()
-st.header("Histogram of Generated Data")
+st.markdown(
+    "<h1 style='text-align:center; color:#333;'>Histogram of Generated Normal Distribution</h1>",
+    unsafe_allow_html=True,
+)
 st.pyplot(fig)
 
-# Download as CSV
-if st.button("Download Data as CSV"):
+# Button styling
+if st.button("Download Data as CSV", key="download_button"):
     df = pd.DataFrame(data, columns=["Value"])
     csv = df.to_csv(index=False)
-    st.download_button("Download CSV", data=csv, file_name="generated_data.csv")
+    st.download_button(
+        "<span style='color:#fff;'>Download CSV</span>",
+        data=csv,
+        file_name="generated_data.csv",
+        unsafe_allow_html=True,
+    )
 
 # App footer
 st.markdown("---")
-st.markdown("Created by Sourajit Ghosh")
+st.markdown("Created by Sourajit")
